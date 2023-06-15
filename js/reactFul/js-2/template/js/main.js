2bec34fd6ef1027a65b7e1496f3a5c897af826d1
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-06-15 10:19:11 -0600

    working on temperature converter

---
+++ js/reactFul/js-2/template/js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('#convert').addEventListener('click', run);
+
+function run() {
+    const cTemperature = parseFloat(document.querySelector('#celsius').value);
+
+    if (isNaN(cTemperature)) {
+        document.querySelector('#placeToYell').innerText = "Please enter a valid temperature.";
+        return;
+    }
+
+    const convertTemp = (cTemperature * 9 / 5) + 32;
+
+    document.querySelector('#placeToYell').innerText = convertTemp;
+}
