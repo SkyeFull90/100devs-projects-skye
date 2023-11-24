dda66abadfa8d8a16c3758117d893acf2db65f27
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-23 20:21:54 -0700

    Fix bugs and update dependencies

--- class_stuff/class20-materials_/class20-materials /review-in-paradise/js/main.js
+++ class_stuff/class20-materials_/class20-materials /review-in-paradise/js/main.js
@@ -1,12 +1,18 @@
 // *Variables*
 // Declare a variable, reassign it to your favorite food, and alert the value
-
+let favoriteFood = "pizza";
+favoriteFood = "tacos";
+alert(favoriteFood);
 //Declare a variable, assign it a string, alert the second character in the string (Use your google-fu and the MDN)
-
+let myString = "Hello";
+alert(myString[1]);
 
 // *Functions*
 // Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last. Alert the product. Call the function.
-
+function myFunction(num1, num2, num3) {
+    alert((num1 / num2) * num3);
+}
+myFunction(10, 5, 2);
 
 // Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
 
