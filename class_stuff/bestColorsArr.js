37bf139827b06b41bbacfd2d1f94ece9a468b8cc
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-26 13:50:37 -0700

    Remove unnecessary files and dependencies

--- class_stuff/bestColorsArr.js
+++
@@ -1,3 +0,0 @@
-let bestColors = ['green', 'blue', 'yellow', 'black']
-
-bestColors.forEach((x,i,a)=> console.log(i,x,a))
--- class_stuff/test_class_work/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-function assignFavoriteFood(food) {
-    let myFavoriteFood = food;
-    //alert(myFavoriteFood);
-    console.log(myFavoriteFood);
-    return myFavoriteFood;
-}
-
-module.exports = { assignFavoriteFood };
