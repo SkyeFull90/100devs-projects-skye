ed40ad008b03f188894071719abd089112fe82e7
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-27 20:31:43 -0700

    Add chai and mocha as devDependencies, and create musketeers.js file

---
+++ js/the_js_way/musketeers.js
@@ -0,0 +1,13 @@
+let musketeers = ["Athos", "Porthos", "Aramis"];
+for (let i = 0; i < musketeers.length; i++) {
+    console.log(musketeers[i]);
+}
+musketeers.push("D'Artagnan");
+musketeers.forEach(myElement => {
+    console.log(`${myElement}`);
+}
+);
+musketeers.splice(2, 1);
+for (let myElement of musketeers) {
+    console.log(myElement);
+}
