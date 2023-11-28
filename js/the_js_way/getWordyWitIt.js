d5fa40f86b1b5acc976a2ff2b819147f898c53fb
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-27 22:48:38 -0700

    Add getWordyWitIt.js and sumArray function

---
+++ js/the_js_way/getWordyWitIt.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function getWords(callback) {
+    let words = [];
+
+    function ask() {
+        rl.question("Enter a word (or 'stop' to finish):", (word) => {
+            if (word === "stop") {
+                callback(words);
+                rl.close();
+            } else {
+                words.push(word);
+                ask();
+            }
+        });
+    }
+
+    ask();
+}
+
+getWords((words) => {
+    for (let word of words) {
+        console.log(word);
+    }
+});
--- js/the_js_way/index.ts
+++ js/the_js_way/index.ts
@@ -1,1 +1,11 @@
 console.log("Hello via Bun!");
+
+function sumArray(values: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < values.length; i++) {
+        sum += values[i];
+    }
+    return sum;
+}
+
+console.log(`The sum of the array is ${sumArray([1, 2, 3, 4, 5])}`);
