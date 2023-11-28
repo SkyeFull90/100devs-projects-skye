a7cdc8e1642bf612671ed9d5a90f5416e34198ed
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-27 21:11:23 -0700

    Add maxArray function to calculate the maximum value in an array

--- js/the_js_way/sumval.js
+++ js/the_js_way/sumval.js
@@ -7,3 +7,15 @@ function sumArray(values) {
 }
 
 console.log(`The sum of the array is ${sumArray([1, 2, 3, 4, 5])}`);
+
+function maxArray(values) {
+    let max = values[0];
+    for (let i = 1; i < values.length; i++) {
+        if (values[i] > max) {
+            max = values[i];
+        }
+    }
+    return max;
+}
+
+console.log(`The max of the array is ${maxArray([1, 2, 3, 4, 5])}`);
