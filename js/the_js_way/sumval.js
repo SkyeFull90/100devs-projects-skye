431f018f159852c07cc906929a7d0b12229e8204
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-27 20:56:00 -0700

    Refactor sum calculation to use a function

--- js/the_js_way/sumval.js
+++ js/the_js_way/sumval.js
@@ -1,6 +1,9 @@
-const values = [1, 2, 3, 4, 5];
-let sum = 0;
-for (let i = 0; i < values.length; i++) {
-    sum += values[i];
+function sumArray(values) {
+    let sum = 0;
+    for (let i = 0; i < values.length; i++) {
+        sum += values[i];
+    }
+    return sum;
 }
-console.log(`The sum of the array is ${sum}`);
+
+console.log(`The sum of the array is ${sumArray([1, 2, 3, 4, 5])}`);
