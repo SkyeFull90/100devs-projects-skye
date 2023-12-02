13601a31545c39e5a3d74c557337c743ebf2c3cb
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-01 23:30:37 -0700

    Added codeWars project files

---
+++ codeWars/tests/fakeBinary.test.js
@@ -0,0 +1,14 @@
+import chai from "chai";
+import {describe, it} from "bun:test";
+
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+const fakeBin = require("../fakeBinary.js");
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(fakeBin('45385593107843568'), '01011110001100111');
+    assert.strictEqual(fakeBin('509321967506747'), '101000111101101'); 
+    assert.strictEqual(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
+  })
+});
