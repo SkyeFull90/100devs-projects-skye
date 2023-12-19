0638bb849430e9076ede8603bdd94651928bf4a1
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-19 13:24:37 -0700

    Refactor object_oriented_programming/index.js

--- js/object_oriented_programming/index.js
+++ js/object_oriented_programming/index.js
@@ -1,3 +1,37 @@
 const cirlce = {
-    
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log('draw');
+    }
 }
+
+cirlce.draw();  // Outputs: draw
+// Factory Function
+
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw: function() {
+//             console.log('draw');
+//         }
+//     };
+// }
+
+// const circle = createCircle(1);
+// circle.draw();  // Outputs: draw
+// Constructor Function
+
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// const another = new Circle(1);
+// another.draw();  // Outputs: draw
+// Dynamic Nature of Objects
