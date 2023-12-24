8fee66015674559b92534e3d98bd2973325980f6
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-23 20:43:02 -0700

    Update mouse object and add constructor function

--- class_stuff/class22-materials/class22-materials/objects-street-fighter/js/main.js
+++ class_stuff/class22-materials/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,18 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function StreetFighter(name, health, attack,defense){
+    this.name = name;
+    this.health = health;
+    this.attack = attack;
+    this.defense = defense;
+    this.fight = function(){
+        console.log(`${this.name} is fighting`);
+    }
+}
+
+let ryu = new StreetFighter("Ryu", 100, 50, 50);
+let ken = new StreetFighter("Ken", 100, 50, 50);
+let chunLi = new StreetFighter("Chun Li", 100, 50, 50);
+let blanka = new StreetFighter("Blanka", 100, 50, 50);
+let zangief = new StreetFighter("Zangief", 100, 50, 50);
+console.log(ryu);
+ryu.fight();
--- js/object_oriented_programming/index.js
+++ js/object_oriented_programming/index.js
@@ -11,27 +11,39 @@ const cirlce = {
 
 cirlce.draw();  // Outputs: draw
 // Factory Function
+/*
 
-// function createCircle(radius) {
-//     return {
-//         radius,
-//         draw: function() {
-//             console.log('draw');
-//         }
-//     };
-// }
+*/
+ function createCircle(radius) {
+     return {
+         radius,
+         draw: function() {
+             console.log('draw');
+         }
+     };
+ }
 
-// const circle = createCircle(1);
-// circle.draw();  // Outputs: draw
+ const circle = createCircle(1);
+ circle.draw();  // Outputs: draw
 // Constructor Function
+/*
 
-// function Circle(radius) {
-//     this.radius = radius;
-//     this.draw = function() {
-//         console.log('draw');
-//     }
-// }
-
-// const another = new Circle(1);
-// another.draw();  // Outputs: draw
+*/
+ function Circle(radius) {
+     this.radius = radius;
+     this.draw = function() {
+         console.log('draw');
+     }
+ }
+ 
+ const another = new Circle(1);
+ another.draw();  // Outputs: draw
 // Dynamic Nature of Objects
+let obj = { value: 10 };
+
+function increase(obj) {
+    obj.value++;
+}
+
+increase(obj);
+console.log(obj);  // Outputs: { value: 11 }
