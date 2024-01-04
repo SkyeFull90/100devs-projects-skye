e38f405c4b3f6007fa251f2527692ee5c77591ba
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-04 08:36:36 -0700

    Add sumArr and squaredArr functions

--- class_stuff/class23-materials/class23-materials/space-review/js/main.js
+++ class_stuff/class23-materials/class23-materials/space-review/js/main.js
@@ -1,10 +1,19 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
-
+ 
+function sumArr(arr){
+    return arr.reduce((a,b) => a+b, 0);
+}
+let arr = [4,9,7,17]
+//alert(sumArr(arr));
+console.log(sumArr(arr));
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
-
+function squaredArr(arr){
+    return arr.map(x => x*x);
+}
+console.log(squaredArr(arr));
 //Create a function that takes string
 //Print the reverse of that string to the console
 
