ebeebcdf8fbbc5d0f592eab8be0689895bca8b7e
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-09 22:28:45 -0700

    Add Tony Hawk Pro Skater constructor and methods

--- class_stuff/class23-materials/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class_stuff/class23-materials/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,24 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function TonyHawkProSkater(name, age, height, weight) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.kickflip = function () {
+        console.log("Kickflip!");
+    };
+    this.grind = function () {
+        console.log("Grinding!");
+    };
+    this.manual = function () {
+        console.log("Manual!");
+    };
+}
+
+let tonyHawk = new TonyHawkProSkater("Tony Hawk", 52, "6'3", 180);
+console.log(tonyHawk);
+tonyHawk.kickflip();
+tonyHawk.grind();
+tonyHawk.manual();
+// TODO refactor and practice this more
