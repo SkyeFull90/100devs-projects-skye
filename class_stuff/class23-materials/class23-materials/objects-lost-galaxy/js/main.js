872e9140dde9d3647c6be59c12ac6c978e6aba1d
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-09 22:16:11 -0700

    Add dog and car objects with properties and methods

--- class_stuff/class23-materials/class23-materials/objects-lost-galaxy/js/main.js
+++ class_stuff/class23-materials/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,48 @@
 //Create a dog object that has four properties and three methods
+let dog = {
+    name: "Buddy",
+    age: 3,
+    breed: "Golden Retriever",
+    color: "Golden",
+    bark: function () {
+        console.log("Woof!");
+    },
+    eat: function () {
+        console.log("Nom nom nom");
+    }
+};
+
+/* 
+    Create a car object that has four properties and three methods
+    function start() {
+        console.log("Starting car...");
+    }
+    function drive() {
+        console.log("Driving car...");
+    }
+    function stop() {
+        console.log("Stopping car...");
+    }
+
+    let car = {
+        make: "Honda",
+        model: "Civic",
+        year: 2010,
+        color: "Black",
+        start: start,
+        drive: drive,
+        stop: stop
+    }
+
+    car.start();
+    car.drive();
+    car.stop();
+
+    
+
+
+
+
+
+
+*/
