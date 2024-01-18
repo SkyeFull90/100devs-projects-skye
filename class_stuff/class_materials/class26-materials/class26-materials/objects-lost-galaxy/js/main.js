8d5c01ff600150be2b4eeed0326920d3a978ed51
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-18 12:21:02 -0700

    Add Dog class with properties and methods

--- class_stuff/class_materials/class26-materials/class26-materials/objects-lost-galaxy/js/main.js
+++ class_stuff/class_materials/class26-materials/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,16 @@
 //Create a dog object that has four properties and three methods
+function Dog(name, breed, age, color) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.color = color;
+    this.bark = function () {
+        console.log("Bark!");
+    };
+    this.eat = function () {
+        console.log("Eat!");
+    };
+    this.sleep = function () {
+        console.log("Sleep!");
+    };
+}
