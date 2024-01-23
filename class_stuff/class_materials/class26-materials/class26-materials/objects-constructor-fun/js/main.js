357754b8b65411f06428a25b25370ca473541126
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-22 22:50:04 -0700

    Add PizzaMachine constructor with properties and methods

--- class_stuff/class_materials/class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ class_stuff/class_materials/class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,16 @@
 //Create a constructor with 4 properties and 3 methods
+function PizzaMachine(name, size, price, toppings) {
+    this.name = name;
+    this.size = size;
+    this.price = price;
+    this.toppings = toppings;
+    this.addTopping = function (topping) {
+        this.toppings.push(topping);
+    }
+    this.removeTopping = function (topping) {
+        this.toppings.splice(this.toppings.indexOf(topping), 1);
+    }
+    this.changePrice = function (newPrice) {
+        this.price = newPrice;
+    }
+}
