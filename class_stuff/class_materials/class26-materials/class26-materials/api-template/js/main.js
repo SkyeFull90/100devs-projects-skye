75a59414a139fadadef403ec9442fd7d8873951a
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-25 21:48:05 -0700

    Refactor cocktail API fetch function

--- class_stuff/class_materials/class26-materials/class26-materials/api-template/js/main.js
+++ class_stuff/class_materials/class26-materials/class26-materials/api-template/js/main.js
@@ -16,15 +16,21 @@ function getFetch(){
 }
 */
 
-document.querySelector('#get-cocktail').addEventListener('click', () => {
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(data => {
-      const cocktail = data.drinks[0];
+document.querySelector('button').addEventListener('click', getDrink)
 
-      document.querySelector('#cocktail-name').textContent = cocktail.strDrink;
-      document.querySelector('#cocktail-photo').src = cocktail.strDrinkThumb;
-      document.querySelector('#cocktail-instructions').textContent = cocktail.strInstructions;
-    })
-    .catch(err => console.log(err));
-});
+function getDrink(){
+  const drink = document.querySelector('input').value
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('p').innerText = data.drinks[0].strInstructions
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
