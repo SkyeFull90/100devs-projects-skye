072a33bb2c1285794bb58b702f60308aabc9ce79
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-08 19:37:22 -0700

    Add getNasaPicture function to fetch NASA picture of the day

--- class_stuff/class_materials/class26-materials/class26-materials/objects-nasa/js/main.js
+++ class_stuff/class_materials/class26-materials/class26-materials/objects-nasa/js/main.js
@@ -1,2 +1,24 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+function getNasaPicture(date) {
+    const apiKey = ''
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // use the data here
+            console.log(data);
+        })
+        .catch(error => console.error('Error:', error));
+}
 
+// main.js
+const apiKey = 'YOUR_NASA_API_KEY'; // replace with your API key
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        // use the data here
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
