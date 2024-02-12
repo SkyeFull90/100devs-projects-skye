f246030fd20bf8f08fda18aac658c55bc0220ef5
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-12 13:45:42 -0700

    Update API template with APOD functionality

--- class_stuff/class_materials/class27-materials/class27-materials/api-template/js/main.js
+++ class_stuff/class_materials/class27-materials/class27-materials/api-template/js/main.js
@@ -2,13 +2,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.getElementById('explanation').innerText = data.explanation
+        document.getElementById('title').innerText = data.title
+        document.getElementById('copyright').innerText = data.copyright
       })
       .catch(err => {
           console.log(`error ${err}`)
