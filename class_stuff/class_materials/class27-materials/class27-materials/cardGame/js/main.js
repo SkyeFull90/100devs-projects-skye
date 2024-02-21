63fd9d0d64ab07c99ca360b6201fc8fa8c526d55
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-21 08:51:31 -0700

    Refactor fetchDeck function and add getFetch function

--- class_stuff/class_materials/class27-materials/class27-materials/cardGame/js/main.js
+++ class_stuff/class_materials/class27-materials/class27-materials/cardGame/js/main.js
@@ -1,21 +1,37 @@
 //Create a button that fetches a new deck from the deck of cards API and stores the deck id in localStorage
 let deckId = ''
 
-/*
-document.querySelector('button').addEventListener('click', fetchDeck)
+document.querySelector('button').addEventListener('click', getFetch)
 
-function fetchDeck(){
-    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(response => response.json())
-    .then(data => {
-        deckId = data.deck_id
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
         console.log(data)
-        localStorage.setItem('deckId', deckId)
-    })
+        if(data.media_type === 'video'){
+          document.querySelector
+          ('iframe').src = data.url
+        }
+        else{
+          document.querySelector('img').src = data.hdurl
+        }
+        document.getElementById('explanation').innerText = data.explanation
+        document.getElementById('title').innerText = data.title
+        if(data.copyright){
+          document.getElementById('copyright').innerText = data.copyright
+        }
+        else{
+          document.getElementById('copyright').innerText = ''
+        }
+        
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 }
-*/
-//Create a button that draws 2 cards from the deck in localStorage and stores the drawn cards in localStorage
-
 
 
 
