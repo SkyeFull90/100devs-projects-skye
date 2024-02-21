51e07121dd3c162e0b682630d3237a3facc862b8
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-21 15:36:45 -0700

    Update fetch URL in main.js

--- class_stuff/class_materials/class27-materials/class27-materials/cardGame/js/main.js
+++ class_stuff/class_materials/class27-materials/class27-materials/cardGame/js/main.js
@@ -2,7 +2,7 @@
 
 let deckId = ''
 
-fetch(url)
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     .then(res => res.json()) // parse response as JSON
     .then(data => {
         console.log(data)
