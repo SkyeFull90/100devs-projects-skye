20c68107c26bae3aa04b7950591fd0bd3ca690c0
taiko <full.skyl@hotamil.com>
2024-02-22 19:24:25 -0700

    Update card game UI and logic

--- class_stuff/class_materials/class27-materials/class27-materials/cardGame/js/main.js
+++ class_stuff/class_materials/class27-materials/class27-materials/cardGame/js/main.js
@@ -25,8 +25,33 @@ function drawTwo(){
         console.log(data)
         document.querySelector('#player1').src = data.cards[0].image
         document.querySelector('#player2').src = data.cards[1].image
+
+        let player1 = convertNum(data.cards[0].value) 
+        let player2 = convertNum(data.cards[1].value) 
+
+        if(player1 === player2){
+          document.querySelector('h2').innerText = 'It\'s a tie!'
+        } else if(player1 > player2){
+          document.querySelector('h2').innerText = 'Player 1 wins!'
+        } else {
+          document.querySelector('h2').innerText = 'Player 2 wins!'
+        }
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+function convertNum(val){
+  if(val === 'JACK'){
+    return 11
+  } else if(val === 'QUEEN'){
+    return 12
+  } else if(val === 'KING'){
+    return 13
+  } else if(val === 'ACE'){
+    return 14
+  } else {
+    return Number(val)
+  }
 }
