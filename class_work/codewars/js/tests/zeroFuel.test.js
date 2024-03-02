99e232adfcaf38fc7ba50b1784293cc36381cbc4
skye <dull.skyl@hotmail.com>
2024-03-02 08:52:29 -0700

    Add JavaScript project files and dependencies

---
+++ class_work/codewars/js/tests/zeroFuel.test.js
@@ -0,0 +1,9 @@
+const assert = require("chai").assert;
+const zeroFuel = require("../zeroFuel");
+
+describe("zeroFill", function() {
+  it("Sample Tests", function() {
+    assert.equal(zeroFuel(50, 25, 2), true);
+    assert.equal(zeroFuel(100, 50, 1), false);
+  });
+});
