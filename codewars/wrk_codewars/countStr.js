2b827a1e75e9fd3472b96593e3f7c3d99151360c
skye <dull.skyl@hotmail.com>
2024-03-05 12:10:07 -0700

    Fix buffer encoding issue

--- codewars/wrk_codewars/countStr.js
+++ codewars/wrk_codewars/countStr.js
@@ -10,3 +10,7 @@ function newUint8Array(number) {
 const buffer = newUint8Array(2**16).fill("a".codePointAt(0));
 
 const str = new TextDecoder().decode(buffer);
+//console.log(str); // a
+
+const buffer2 = new TextEncoder().encode(str);
+console.log(buffer2); // Uint8Array [ 97 ]
