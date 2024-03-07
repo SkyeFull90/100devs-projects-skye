edc847521017b7e9567d2d5fd101fcb579be71f1
skye <dull.skyl@hotmail.com>
2024-03-07 11:51:46 -0700

    Add duplicate count function implementation in JavaScript and Python

---
+++ codewars/js/dupCount.cjs
@@ -0,0 +1,14 @@
+function duplicateCount(text) {
+    let count = 0;
+    let textArr = text.toLowerCase().split('');
+    let uniqueArr = [];
+    textArr.forEach((char, index) => {
+        if (textArr.indexOf(char) !== index && !uniqueArr.includes(char)) {
+            count++;
+            uniqueArr.push(char);
+        }
+    });
+    return count;
+
+}
+console.log(duplicateCount("Hello World"));
