b803d810eaeb7048fe1cc11a591e3345560c9b42
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-03-11 20:33:55 -0600

    Update HTML and JavaScript to display Pokemon name and author name

--- class_work/class_materials/class28-materials/class28-materials/api-template copy/js/main.js
+++ class_work/class_materials/class28-materials/class28-materials/api-template copy/js/main.js
@@ -10,7 +10,8 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('li').innerText = data.docs[0].title
+        document.querySelector('h3').innerText = data.docs[0].title
+        document.querySelector('h4').innerText = data.docs[0].author_name
       })
       .catch(err => {
           console.log(`error ${err}`)
