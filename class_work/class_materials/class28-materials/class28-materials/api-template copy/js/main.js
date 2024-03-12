c25bfafac0100b8ef025b6545cde469eba7c657a
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-03-11 19:55:26 -0600

    Add API template files and styles

---
+++ class_work/class_materials/class28-materials/class28-materials/api-template copy/js/main.js
@@ -0,0 +1,18 @@
+// Fetch using nasa apod api
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
