49602cc89c0af018dfc74d6b54a6d8a6683a6f93
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-03-11 20:06:20 -0600

    Update API endpoint and display name and type of Pokemon

--- class_work/class_materials/class28-materials/class28-materials/api-template copy/js/main.js
+++ class_work/class_materials/class28-materials/class28-materials/api-template copy/js/main.js
@@ -4,7 +4,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+  const url = `https://openlibrary.org/search.json?title=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
