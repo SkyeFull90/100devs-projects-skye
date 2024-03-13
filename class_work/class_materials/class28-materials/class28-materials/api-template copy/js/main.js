bf54f4d2bd2159cc82d1e43c054d5e9010a40573
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-03-12 19:51:51 -0600

    Update API URL and display data correctly

--- class_work/class_materials/class28-materials/class28-materials/api-template copy/js/main.js
+++ class_work/class_materials/class28-materials/class28-materials/api-template copy/js/main.js
@@ -4,14 +4,14 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://openlibrary.org/search.json?title=${choice}`
+  const url = `https://openlibrary.org/isbn/${choice}.json`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('h3').innerText = data.docs[0].title
-        document.querySelector('h4').innerText = data.docs[0].author_name
+        document.querySelector('h3').innerText = data.title
+        document.querySelector('h4').innerText = data.description
       })
       .catch(err => {
           console.log(`error ${err}`)
