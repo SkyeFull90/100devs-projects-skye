1d6353fe4d6f962096357f36dee4d251434c89c0
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-04-01 18:10:35 -0600

    Add switchItUp function and test cases

---
+++ codewars/js/tests/switchItUp.test.js
@@ -0,0 +1,11 @@
+import {assert} from "chai";
+
+import switchItUp from "../switchItUp.cjs";
+
+describe("Basic Tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(switchItUp(1),"One");
+    assert.strictEqual(switchItUp(3),"Three");
+    assert.strictEqual(switchItUp(5),"Five");
+  });
+});
