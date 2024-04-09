1080835e05f4dc14afc2e60db9f2c67ae5fe6945
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-04-08 18:09:19 -0600

    Refactor main.js to reverse Pokémon party order and add function to compare sum of squares and cubes

--- class_work/class_materials/class29-materials/class29-materials/cascade-badge/js/main.js
+++ class_work/class_materials/class29-materials/class29-materials/cascade-badge/js/main.js
@@ -1,9 +1,15 @@
 //Arrays
-//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+//Your Pokémon party order which is a list of Pokémon has been leaked to Misty.
+// Please create a function that reverses your list and prints it to the console.
+function reversePokemonOrder(pokemonParty) {
+    return pokemonParty.reverse();
+}
+console.log(reversePokemonOrder(["Pikachu", "Charmander", "Bulbasaur", "Squirtle"]));
+//Given two integer arrays a, b, both of lengths >= 1,
+// create a program that returns true
+// if the sum of the squares of each element in an is strictly greater than the sum of the cubes of each element in b.
 
-//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
-
-//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+//Return a new array consisting of elements which are multiple of their own index in an input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
