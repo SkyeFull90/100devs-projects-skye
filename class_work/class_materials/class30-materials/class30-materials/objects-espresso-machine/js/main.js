908efd07df50e4b803c7c12d82fd0b99579ccc3a
Zoe <full.skyl@hotmail.com>
2024-06-05 15:18:35 -0600

    working on class 30 espresso macine constructor

--- class_work/class_materials/class30-materials/class30-materials/objects-espresso-machine/js/main.js
+++ class_work/class_materials/class30-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,10 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class espressoMachine{
+    constructor(grind, press, brew, serve){
+        this.grind = espressoMachine
+        this.press = espressoMachine
+        this.brew = espressoMachine
+        this.serve = espressoMachine
+    }
+
+}
