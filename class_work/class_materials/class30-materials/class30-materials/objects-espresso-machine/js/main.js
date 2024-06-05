7b9f805a7877da786e1503d6f9aea192d9bbd965
Zoe <full.skyl@hotmail.com>
2024-06-05 15:42:22 -0600

    modified main.js

--- class_work/class_materials/class30-materials/class30-materials/objects-espresso-machine/js/main.js
+++ class_work/class_materials/class30-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -1,10 +1,18 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
 class espressoMachine{
-    constructor(grind, press, brew, serve){
-        this.grind = espressoMachine
-        this.press = espressoMachine
-        this.brew = espressoMachine
-        this.serve = espressoMachine
+    constructor(make, price, model, color){
+        this.make = make
+        this.price = price
+        this.model = model
+        this.color = color
     }
-
+    turnOn(){
+        console.log("Good day, what are we drinking today?")
+    }
+    steam(){
+        console.log("ITSSS SUMMERR SALEEEEEEEEE 50% OFFFFFFF!")
+    }
+    brew(){
+        console.log("your drink is now brewing.")
+    }
 }
