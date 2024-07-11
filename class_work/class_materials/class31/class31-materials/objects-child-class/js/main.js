572a9a2cb9a934ae3310eb8490f4def271cfb3bf
taiko <full.skyl@hotmail.com>
2024-07-10 20:21:49 -0600

    created the anime class and extended it with the series class

--- class_work/class_materials/class31/class31-materials/objects-child-class/js/main.js
+++ class_work/class_materials/class31/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,12 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
+class Anime{
+    constructor(name){
+        this.name = name
+    }
+    weeb(){
+        console.log(`${this.name} is a good anime`)
+    }
+}
+class Series extends Anime{
 
+}
