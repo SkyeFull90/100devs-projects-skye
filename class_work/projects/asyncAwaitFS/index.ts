4c5c9e1797a8be3606da357ed037e4384e82a86f
taiko <full.skyl@hotmail.com>
2024-07-23 11:42:02 -0600

    added a exmaple for async await

--- class_work/projects/asyncAwaitFS/index.ts
+++ class_work/projects/asyncAwaitFS/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+// console.log("Hello via Bun!");
+
+const promise = fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+promise
+    .then(res => res.json())
+    .then(user => console.log('smile', user.title));
+
+console.log('block Synchronous')    
