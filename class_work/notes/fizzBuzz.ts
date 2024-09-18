05ba9e8211d50567b430126eceeb443c5627c746
taiko <full.skyl@hotmail.com>
2024-09-18 10:50:18 -0600

    modified fizzBuzz.ts

--- class_work/notes/fizzBuzz.ts
+++ class_work/notes/fizzBuzz.ts
@@ -3,7 +3,11 @@ function fizzBuzz(num: number): string[] {
     //loop
     for(let i = 1; i<= num; i++){
     //conditions % 3&5, % 3, % 5
-        
+        if(i % 3 && i % 5){
+            answer.push("FizBuzz")
+        } else if(i % 3){
+            answer.push("Fizz")
+        }
     }
     //console log the result
     return answer;
