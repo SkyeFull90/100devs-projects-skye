4744c8cd9196527316274ab4316f405e30fd382f
taiko <full.skyl@hotmail.com>
2024-09-18 12:17:49 -0600

    modified fizzBuzz

--- class_work/notes/fizzBuzz.ts
+++ class_work/notes/fizzBuzz.ts
@@ -1,4 +1,4 @@
-function fizzBuzz(num: number): string[] {
+function fizzbuzz(num: number): string[] {
     let answer: string[] = [];
     //loop
     for(let i = 1; i<= num; i++){
@@ -7,13 +7,19 @@ function fizzBuzz(num: number): string[] {
             answer.push("FizBuzz")
         } else if(i % 3){
             answer.push("Fizz")
+        } else if(i % 5){
+            answer.push("Buzz")
+        } else {
+            answer.push(i.toString())
         }
     }
     //console log the result
     return answer;
 }
-console.log(fizzBuzz(5))
+console.log(fizzbuzz(5))
 
-console.log(fizzBuzz(3))
+console.log(fizzbuzz(3))
 
-console.log(fizzBuzz(15))
+console.log(fizzbuzz(15))
+
+export default fizzbuzz;
