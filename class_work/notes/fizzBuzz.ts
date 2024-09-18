3ad13e010d8a49533bc13fd46e872ecc30c61423
taiko <full.skyl@hotmail.com>
2024-09-17 22:51:34 -0600

    modified fizzBuzz.js file to be a typescript file

---
+++ class_work/notes/fizzBuzz.ts
@@ -0,0 +1,12 @@
+function fizzBuzz(num: number): string[] {
+    let answer: string[] = [];
+    for(let i = 1; i<= num; i++){
+        
+    }
+    return answer;
+}
+console.log(fizzBuzz(5))
+
+console.log(fizzBuzz(3))
+
+console.log(fizzBuzz(15))
