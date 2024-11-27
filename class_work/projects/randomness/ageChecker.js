f961e63b1bf00ed062e1c0e77c5251d46c60426b
nyx <full.skyl@hotmail.com>
2024-11-26 18:43:48 -0700

    modified ageChecker.js

--- class_work/projects/randomness/ageChecker.js
+++ class_work/projects/randomness/ageChecker.js
@@ -1,32 +1,19 @@
-/*function ageChecker(){
-    
+function ageChecker(age) {
+  const drivingAge = 16;
+  const drinkingAge = 21;
+  const affordableRentalAge = 25;
 
-    if (age < 16) {
-        console.log("You can drive")
-    }
-    else if(age < 18){
-        console.log("You can't hate from outside the club, because you can't get in")
-    } 
-
-    else if(age < 21){
-        console.log("You can drink")
-    } 
-    else if(age < 25){
-        console.log("You can't hate from outside the club, because you can't get in")
-    } 
-    else if(age < 30){
-        console.log("You can't hate from outside the club, because you can't get in")
-    } 
-    else {
-        console.log("You old")
-    } 
+  if (age >= affordableRentalAge) {
+    console.log("You can rent a car affordably.");
+  } else if (age >= drinkingAge) {
+    console.log("You are old enough to drink.");
+  } else if (age >= drivingAge) {
+    console.log("You are old enough to drive.");
+  } else {
+    console.log("You are too young for all of the above.");
+  }
 }
-*/
-const birthYear = 2000;
-const isAdult = birthYear;
 
-if (isAdult) {
-  console.log("You are of legal age.");
-} else {
-  console.log("You are not yet of legal age.");
-}
+// Example usage:
+let age = 23;
+ageChecker(age);
