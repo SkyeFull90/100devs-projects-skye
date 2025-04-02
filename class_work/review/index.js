59f2c36572f1e2b3ff7a50869386bbb9b3d8d931
nyx <full.skyl@hotmail.com>
2025-04-02 14:27:08 -0600

    Add basic math functions for division and conditional addition
    Created a `divide` function to compute the quotient of two numbers. Added an `add` function that logs the sum if it exceeds 5.

---
+++ class_work/review/index.js
@@ -0,0 +1,15 @@
+// Create a function that divides one num by another and returns the remainder
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+console.log(divide(10, 5));
+
+// create a function that adds two numbers and if the sum is greater than 5 log the result
+
+function add(num1, num2) {
+    if (num1 + num2 > 5) {
+        console.log(num1 + num2);
+    }
+}
+add(10, 5);
