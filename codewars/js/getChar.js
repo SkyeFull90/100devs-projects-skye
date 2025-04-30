4ef8db95155fa11ddea12b8c0f86be0f6dc8e797
nyx <full.skyl@hotmail.com>
2025-04-30 11:21:01 -0600

    Add `getChar` function and corresponding unit tests
    Implemented a function to convert a number to its ASCII character using `String.fromCharCode`. Added unit tests with various inputs to ensure correct functionality and coverage.

---
+++ codewars/js/getChar.js
@@ -0,0 +1,9 @@
+// write a function which takes a number abd returns the corresponding ASCII char
+
+function getChar(c) {
+    return String.fromCharCode(c);
+}
+
+console.log(getChar(65))
+
+export default getChar;
