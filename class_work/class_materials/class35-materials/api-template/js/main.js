5d66c366e4377190505fc3f103e80cc1bb1c9aba
halokiller36 <halokiller36@gmail.com>
2025-09-30 18:53:54 +0000

    added a api request for a dog api to the index.html

--- class_work/class_materials/class35-materials/api-template/js/main.js
+++ class_work/class_materials/class35-materials/api-template/js/main.js
@@ -1,1 +1,8 @@
 //Make an api request using async await
+document.querySelector('button').addEventListener('click', getDog)
+
+async function getDog(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    document.querySelector('img').src = data.message
+}
