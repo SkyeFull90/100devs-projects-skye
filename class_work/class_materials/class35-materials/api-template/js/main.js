fe7b5acf6280eec3d65dbac0c579d11d90c1695e
nyx <full.skyl@hotmail.com>
2025-10-03 22:37:06 -0600

    refactor: comment out alternative API request methods in main.js

--- class_work/class_materials/class35-materials/api-template/js/main.js
+++ class_work/class_materials/class35-materials/api-template/js/main.js
@@ -6,3 +6,30 @@ async function getDog(){
     const data = await res.json()
     document.querySelector('img').src = data.message
 }
+//Make an api request using promises
+// document.querySelector('button').addEventListener('click', getDog)
+
+// function getDog(){
+//     fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(res => res.json())
+//     .then(data => {
+//         document.querySelector('img').src = data.message
+//     })
+// }
+
+//Make an api request using reqres.append('field', value);
+// document.querySelector('button').addEventListener('click', getDog)
+
+// function getDog(){
+//     const xhr = new XMLHttpRequest()
+//     xhr.open('GET', 'https://dog.ceo/api/breeds/image/random')
+//     xhr.onload = function(){
+//         if(xhr.status === 200){
+//             const data = JSON.parse(xhr.responseText)
+//             document.querySelector('img').src = data.message
+//         } else {
+//             console.log('Error: ' + xhr.status)
+//         }
+//     }
+//     xhr.send()
+// }
