35ca492cb4aa07da613143c61acf48d72efb7d28
nyx <full.skyl@hotmail.com>
2025-10-19 15:13:43 -0600

    added fizzbuzz.py to notes dir 100devs Projects

---
+++ class_work/notes/fizzBuzz/fizzBuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        else:
+            result.append(str(i))
+    print(result)
+
+fizzbuzz(15)
